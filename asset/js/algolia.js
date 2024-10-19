8f45b4f2599b487aad83ff20eae95d9d6e21928e
Thien Nguyen <hi@thiennguyen.dev>
2024-10-20 03:39:33 +0700

    Added navigation keys for algolia search box

--- asset/js/algolia.js
+++ asset/js/algolia.js
@@ -1,6 +1,11 @@
 /* global instantsearch, algoliasearch */
 // eslint-disable-next-line no-unused-vars
 function loadAlgolia(config, translation) {
+
+  const $main = $('.searchbox');
+  const $input = $main.find('.searchbox-input');
+  const $container = $main.find('.searchbox-body');
+
   const search = instantsearch({
     indexName: config.indexName,
     searchClient: algoliasearch(config.applicationId, config.apiKey),
@@ -89,6 +94,43 @@ function loadAlgolia(config, translation) {
     $('.searchbox').addClass('show');
   }
 
+  document.querySelectorAll('.searchbox-pagination-link').forEach(link => {
+    link.setAttribute('href', '/');
+  });
+
+  function selectItemByDiff(value) {
+    const $items = $.makeArray($container.find('.searchbox-result-item'));
+    let prevPosition = -1;
+    $items.forEach((item, index) => {
+      if ($(item).hasClass('active')) {
+        prevPosition = index;
+      }
+    });
+    const nextPosition = ($items.length + prevPosition + value) % $items.length;
+    $($items[prevPosition]).removeClass('active');
+    $($items[nextPosition]).addClass('active');
+    scrollTo($($items[nextPosition]));
+  }
+
+  function scrollTo($item) {
+    if ($item.length === 0) return;
+    const wrapperHeight = $container[0].clientHeight;
+    const itemTop = $item.position().top - $container.scrollTop();
+    const itemBottom = $item[0].clientHeight + $item.position().top;
+    if (itemBottom > wrapperHeight + $container.scrollTop()) {
+      $container.scrollTop(itemBottom - $container[0].clientHeight);
+    }
+    if (itemTop < 0) {
+      $container.scrollTop($item.position().top);
+    }
+  }
+
+  function gotoLink($item) {
+    if ($item && $item.length) {
+      location.href = $item.attr('href');
+    }
+  }
+
   $(document)
     .on('click', '.navbar-main .search', () => {
       $('.searchbox').toggleClass('show');
@@ -99,5 +141,28 @@ function loadAlgolia(config, translation) {
     })
     .on('click', '.searchbox-close', () => {
       $('.searchbox').removeClass('show');
+    })
+    .on('keydown', (e) => {
+      if (!$main.hasClass('show')) return;
+      switch (e.keyCode) {
+        case 27: // ESC
+          $main.removeClass('show');
+          break;
+        case 38: // UP
+          selectItemByDiff(-1);
+          break;
+        case 40: // DOWN
+          selectItemByDiff(1);
+          break;
+        case 13: // ENTER
+          gotoLink($container.find('.searchbox-result-item.active').eq(0));
+          break;
+      }
+    })
+    .on('touchstart', (e) => {
+      touch = true;
+    })
+    .on('touchmove', (e) => {
+      touch = false;
     });
 }
